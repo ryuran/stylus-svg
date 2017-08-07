2f3d80b07320ff7288bcc5d0d364c9422f00a4e5
Yvain Liechti <yliechti@clever-age.com>
2017-08-07 11:07:43 +0200

    make a cleaner stylus plugin

--- index.js
+++ index.js
@@ -1,100 +1,101 @@
-function stylusSvg(stylus, svgDirs) {
-    var nodes = stylus.nodes;
-    var utils = stylus.utils;
+function stylusSvg(options) {
     var fs = require('fs');
     var path = require('path');
+    var svgDirs = options.svgDirs || ['./'];
 
+    return function svgImport(style) {
+        var nodes = style.nodes;
+        var utils = style.utils;
 
-    function svgImport(pathExpression, stylusExpression) {
-        var svg = pathExpression.nodes[0];
-        // assert that the node (svg) is a String node, passing
-        // the param name for error reporting
-        utils.assertType(svg, 'string', 'svg');
-        var svgPath = svg.string;
+        style.define('svgImport', function svgImport(pathExpression, stylusExpression) {
+            var svg = pathExpression.nodes[0];
+            // assert that the node (svg) is a String node, passing
+            // the param name for error reporting
+            utils.assertType(svg, 'string', 'svg');
+            var svgPath = svg.string;
 
-        var file = utils.lookup(svgPath, svgDirs);
+            var file = utils.lookup(svgPath, svgDirs);
 
-        // Grab bytes necessary to retrieve dimensions.
-        // if this was real you would do this per format,
-        // instead of reading the entire image :)
-        var data = fs.readFileSync(utils.lookup(svgPath, svgDirs), {encoding: 'utf8'});
+            // Grab bytes necessary to retrieve dimensions.
+            // if this was real you would do this per format,
+            // instead of reading the entire image :)
+            var data = fs.readFileSync(utils.lookup(svgPath, svgDirs), {encoding: 'utf8'});
 
-        if (!/xmlns/.test(data)) {
-            data = data.replace('<svg', '<svg xmlns="http://www.w3.org/2000/svg"');
-        }
+            if (!/xmlns/.test(data)) {
+                data = data.replace('<svg', '<svg xmlns="http://www.w3.org/2000/svg"');
+            }
 
-        if (stylusExpression !== undefined) {
-            var css = stylusExpression.nodes[0];
-            utils.assertType(css, 'string', 'css');
-            css = '<style>' + stylus(css.string).render() + '</style>';
-            data = data.replace(/(<svg(?:.*)>)/, "$1" + css);
-        }
+            if (stylusExpression !== undefined) {
+                var css = stylusExpression.nodes[0];
+                utils.assertType(css, 'string', 'css');
+                css = '<style>' + stylus(css.string).render() + '</style>';
+                data = data.replace(/(<svg(?:.*)>)/, "$1" + css);
+            }
 
-        // console.log(data);
+            // console.log(data);
 
-        // remove xml tag
-        data = data.replace(/<\?xml .*\?>/, '');
+            // remove xml tag
+            data = data.replace(/<\?xml .*\?>/, '');
 
-        // Chunk up string in order to avoid
-        // "stack level too deep" error
-        var encoded = '';
-        var slice = 2000;
-        var index = 0;
-        var loops = Math.ceil(data.length / slice);
+            // Chunk up string in order to avoid
+            // "stack level too deep" error
+            var encoded = '';
+            var slice = 2000;
+            var index = 0;
+            var loops = Math.ceil(data.length / slice);
 
-        // console.log('====' + loops + '===');
+            // console.log('====' + loops + '===');
 
-        for (var i = 0; i < loops; i++) {
-            var chunk = data.slice(index, index + slice);
+            for (var i = 0; i < loops; i++) {
+                var chunk = data.slice(index, index + slice);
 
-            // remove Line Breaks
-            chunk = chunk.replace(/(?:\r\n|\n|\r)/gm, ' ');
+                // remove Line Breaks
+                chunk = chunk.replace(/(?:\r\n|\n|\r)/gm, ' ');
 
-            // remove multiple spaces
-            chunk = chunk.replace(/(?: +)/g, ' ');
+                // remove multiple spaces
+                chunk = chunk.replace(/(?: +)/g, ' ');
 
-            // remove spaces between tags
-            chunk = chunk.replace(/> /g, '>');
-            chunk = chunk.replace(/ </g, '<');
+                // remove spaces between tags
+                chunk = chunk.replace(/> /g, '>');
+                chunk = chunk.replace(/ </g, '<');
 
-            // Encode
-            chunk = chunk.replace(/"/g, '\'');
-            chunk = chunk.replace(/%/g, '%25');
-            chunk = chunk.replace(/&/g, '%26');
-            chunk = chunk.replace(/#/g, '%23');
-            chunk = chunk.replace(/{/g, '%7B');
-            chunk = chunk.replace(/}/g, '%7D');
-            chunk = chunk.replace(/</g, '%3C');
-            chunk = chunk.replace(/>/g, '%3E');
+                // Encode
+                chunk = chunk.replace(/"/g, '\'');
+                chunk = chunk.replace(/%/g, '%25');
+                chunk = chunk.replace(/&/g, '%26');
+                chunk = chunk.replace(/#/g, '%23');
+                chunk = chunk.replace(/{/g, '%7B');
+                chunk = chunk.replace(/}/g, '%7D');
+                chunk = chunk.replace(/</g, '%3C');
+                chunk = chunk.replace(/>/g, '%3E');
 
-            // The maybe list
-            //
-            // Keep size and compile time down
-            // … only add on documented fail
+                // The maybe list
+                //
+                // Keep size and compile time down
+                // … only add on documented fail
 
-            // chunk = chunk.replace(/\|/g, '%7C');
-            // chunk = chunk.replace(/\[/g, '%5B');
-            // chunk = chunk.replace(/\]/g, '%5D');
-            // chunk = chunk.replace(/\^/g, '%5E');
-            // chunk = chunk.replace(/`/g, '%60');
-            // chunk = chunk.replace(/;/g, '%3B');
-            // chunk = chunk.replace(/\?/g, '%3F');
-            // chunk = chunk.replace(/:/g, '%3A');
-            // chunk = chunk.replace(/@/g, '%40');
-            // chunk = chunk.replace(/=/g, '%3D');
+                // chunk = chunk.replace(/\|/g, '%7C');
+                // chunk = chunk.replace(/\[/g, '%5B');
+                // chunk = chunk.replace(/\]/g, '%5D');
+                // chunk = chunk.replace(/\^/g, '%5E');
+                // chunk = chunk.replace(/`/g, '%60');
+                // chunk = chunk.replace(/;/g, '%3B');
+                // chunk = chunk.replace(/\?/g, '%3F');
+                // chunk = chunk.replace(/:/g, '%3A');
+                // chunk = chunk.replace(/@/g, '%40');
+                // chunk = chunk.replace(/=/g, '%3D');
 
-            encoded = encoded + chunk;
-            index = index + slice;
-        }
+                encoded = encoded + chunk;
+                index = index + slice;
+            }
 
-        // console.log(encoded);
+            // console.log(encoded);
 
-        var dataUri = 'data:image/svg+xml;utf8,' + encoded;
+            var dataUri = 'data:image/svg+xml;utf8,' + encoded;
 
-        return new nodes.Literal('url("' + dataUri + '")');
-    }
-
-    return svgImport;
+            return new nodes.Literal('url("' + dataUri + '")');
+        });
+    };
 }
 
 module.exports = stylusSvg;
